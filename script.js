37a0b9dff5ca700a39e0680326927373348cd31f
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-08-20 12:40:21 +0200

    add delete logic

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ class Calculator {
   }
 
   delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
 
   }
 
@@ -102,5 +103,10 @@ equalsButton.addEventListener('click', button => {
 
 allClearButton.addEventListener('click', button => {
   calculator.clear()
+  calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+  calculator.delete()
   calculator.updateDisplay()
 })
