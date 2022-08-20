414e6c78a1eb42637f0eb913b29409c71c4aa379
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-08-20 12:36:19 +0200

    add compute logic

--- script.js
+++ script.js
@@ -24,12 +24,38 @@ class Calculator {
 
   chooseOperation(operation) {
     if (this.currentOperand === '') return
+    if (this,this.previousOperand !== '') {
+      this.compute()
+    }
     this.operation = operation
     this.previousOperand = this.currentOperand
     this.currentOperand = ''
   }
 
   compute() {
+    let computation
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+      case '+':
+        computation = prev + current
+        break
+      case '-':
+        computation = prev - current
+        break
+      case '*':
+        computation = prev * current
+        break
+      case '/':
+        computation = prev / current
+        break
+      default:
+        return
+    }
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
 
   }
 
@@ -67,4 +93,9 @@ operationButtons.forEach(button => {
     calculator.chooseOperation(button.innerText)
     calculator.updateDisplay()
   })
+})
+
+equalsButton.addEventListener('click', button => {
+  calculator.compute()
+  calculator.updateDisplay()
 })
